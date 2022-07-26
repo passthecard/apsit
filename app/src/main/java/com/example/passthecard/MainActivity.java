19ff3fe38f48ffdd0fbe59a8cbcd25d934e84468
PassTheCard <apsitpassthecard@gmail.com>
2022-07-26 22:31:21 +0530

    harsnew_basicversion

--- app/src/main/java/com/example/passthecard/MainActivity.java
+++ app/src/main/java/com/example/passthecard/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.passthecard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+
+
+    }
+
+
+
+    public void loginpress( View view)
+    {
+
+        Intent intent = new Intent(this,login.class);
+        startActivity(intent);
+
     }
 }
